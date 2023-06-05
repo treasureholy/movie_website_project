cfe0222583d8a9af6f8dfb3a3c22eef9a7e597c3
jinsung <treasure1209@naver.com>
2023-06-06 00:38:59 +0900

    Feat: 댓글 불러오기 구현

--- src/review.js
+++ src/review.js
@@ -17,3 +17,25 @@ let saveComment = (name, comment, password) => {
   //배열을 다시 localStorage에 저장
   localStorage.setItem("comments", JSON.stringify(comments));
 };
+
+// [댓글 불러오기]
+let loadComments = () => {
+  //이전에 저장된 댓글 데이터를 불러오기
+  let comments = JSON.parse(localStorage.getItem("comments"));
+
+  // 댓글을 표시할 HTML 요소를 선택
+  let commentContainer = document.getElementById("comment-container");
+
+  // 댓글을 HTML에 추가
+  comments.forEach(function (comment) {
+    let commentElement = document.createElement("div");
+    commentElement.innerHTML =
+      "<b>" + comment.name + "</b> : " + comment.comment;
+    commentContainer.appendChild(commentElement);
+  });
+};
+
+// 페이지가 로드될 때 댓글을 불러오기
+window.onload = function () {
+  loadComments();
+};
