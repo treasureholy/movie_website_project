f2b03337b77f0a165daea5370e9c426a1b7282fe
jinsung <treasure1209@naver.com>
2023-06-06 17:27:09 +0900

    Feat: 댓글 삭제 기능 추가

--- src/review.js
+++ src/review.js
@@ -1,8 +1,8 @@
 // [댓글 저장]
 let saveComment = (name, comment, password) => {
-  //이전에 저장된 댓글 데이터를 불러오기
   //JSON.parse : JSON 문자열을 객체로 전개
-  let comments = JSON.parse(localStorage.getItem("comments"));
+  // 이전에 저장된 댓글 데이터 불러오기 또는 빈 배열로 초기화
+  let comments = JSON.parse(localStorage.getItem("comments")) || [];
 
   //새로운 댓글 객체를 생성 (key - value)
   let newComment = {
@@ -21,26 +21,52 @@ let saveComment = (name, comment, password) => {
 // [댓글 불러오기]
 let loadComments = () => {
   //이전에 저장된 댓글 데이터를 불러오기
-  let comments = JSON.parse(localStorage.getItem("comments"));
+  let comments = JSON.parse(localStorage.getItem("comments")) || [];
 
   // 댓글을 표시할 HTML 요소를 선택
   let commentContainer = document.getElementById("comment-container");
 
+  commentContainer.innerHTML = ""; // 이전에 표시된 댓글들을 초기화
+
   // 댓글을 HTML에 추가
-  comments.forEach(function (comment) {
+  comments.forEach(function (comment, index) {
     let commentElement = document.createElement("div"); // 각 댓글 요소를 생성
     commentElement.classList.add("comment"); // "comment" 클래스 추가(css 추가용)
 
     let commentText = document.createElement("span"); // <div> 요소에 댓글내용 <span>요소를 자식으로 추가)
-    commentElement.innerHTML =
-      "<b>" + comment.name + "</b> : " + comment.comment;
+    commentText.innerHTML = "<b>" + comment.name + "</b> : " + comment.comment;
+
+    // 댓글 삭제 버튼 추가
+    let deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "삭제";
+    deleteButton.addEventListener("click", function () {
+      let password = prompt("비밀번호를 입력하세요.");
+
+      if (password !== null && password === comment.password) {
+        if (confirm("댓글을 삭제하시겠습니까?")) {
+          deleteComment(index);
+        }
+      } else {
+        alert("비밀번호가 일치하지 않습니다.");
+      }
+    });
+
     commentElement.appendChild(commentText);
-    // 댓글 요소를 댓글 컨테이너에 추가
-    commentContainer.appendChild(commentElement);
+    commentElement.appendChild(deleteButton);
+    commentContainer.appendChild(commentElement); // 댓글 요소를 댓글 컨테이너에 추가
   });
 };
 
-// 페이지가 로드될 때 댓글을 불러오기
+// 댓글 삭제 함수
+let deleteComment = (index) => {
+  let comments = JSON.parse(localStorage.getItem("comments")) || [];
+
+  comments.splice(index, 1);
+  localStorage.setItem("comments", JSON.stringify(comments));
+  loadComments();
+};
+
+// [페이지가 로드될 때 댓글을 불러오기]
 window.onload = function () {
   loadComments();
 };
