670d0f39a48159328c3b2e112d4c46bc0e3a9143
jinsung <treasure1209@naver.com>
2023-06-07 01:05:14 +0900

    Comment: JS 주석 추가 및 수정

--- src/review.js
+++ src/review.js
@@ -36,9 +36,9 @@ let loadComments = () => {
     let commentText = document.createElement("span"); // <div> 요소에 댓글내용 <span>요소를 자식으로 추가)
     commentText.innerHTML = "<b>" + comment.name + "</b> : " + comment.comment;
 
-    // 수정 삭제 버튼 추가
-    let editButton = document.createElement("button");
-    editButton.innerHTML = "수정";
+    // 댓글 수정 버튼 추가
+    let editButton = document.createElement("button"); //댓글 수정 button 요소 생성
+    editButton.innerHTML = "수정"; //버튼에 표시될 텍스트
     editButton.addEventListener("click", function () {
       let password = prompt("비밀번호를 입력하세요.");
 
@@ -48,7 +48,7 @@ let loadComments = () => {
           comment.comment
         );
         if (newComment !== null) {
-          editComment(index, newComment);
+          editComment(index, newComment); //index : 현재 댓글의 인덱스, newComment : 입력한 수정된 댓글 내용
         }
       } else {
         alert("비밀번호가 일치하지 않습니다.");
@@ -70,15 +70,19 @@ let loadComments = () => {
       }
     });
 
-    commentElement.appendChild(commentText);
-    commentElement.appendChild(deleteButton);
-    commentElement.appendChild(editButton);
-    commentContainer.appendChild(commentElement); // 댓글 요소를 댓글 컨테이너에 추가
+    //commentElement 요소에 자식 요소로 추가
+    commentElement.appendChild(commentText); //댓글 내용 <span> 요소
+    commentElement.appendChild(deleteButton); //댓글 삭제 버튼 요소
+    commentElement.appendChild(editButton); // 댓글 수정 버튼 요소
+    //commentContainer 요소에 commentElement 요소를 자식 요소로 추가
+    commentContainer.appendChild(commentElement);
   });
 };
 
 // 댓글 수정 함수
 let editComment = (index, newComment) => {
+  //로컬 스토리지에서 comments 키의 값 가져오기
+  //가져온 값은 JSON 형식의 문자열이기 때문에  JSON.parse()를 사용하여 JavaScript 객체로 변환
   let comments = JSON.parse(localStorage.getItem("comments")) || [];
 
   comments[index].comment = newComment;
@@ -89,9 +93,10 @@ let editComment = (index, newComment) => {
 
 // 댓글 삭제 함수
 let deleteComment = (index) => {
+  //삭제할 댓글의 인덱스
   let comments = JSON.parse(localStorage.getItem("comments")) || [];
 
-  comments.splice(index, 1);
+  comments.splice(index, 1); //splice() 메서드를 사용 : 배열 특정 위치의 요소를 제거
   localStorage.setItem("comments", JSON.stringify(comments));
   loadComments();
 };
