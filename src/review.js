6e22ead92eb414e65e8ece1cfdaaadabce33be95
hanbyul1025@gmail.com <hanbyul1025@gmail.com>
2023-06-06 14:20:42 +0900

    정렬까지 완료

--- src/review.js
+++ src/review.js
@@ -20,9 +20,52 @@ window.onload = function () {
     $user_password.value = null;
   }
 
+  // 댓글 입력창 기능 구현
+  $_content.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (method === "submit") {
+      // 등록일 때
+      const date = new Date();
+      let text = $write_input.value;
+      let name = $user_name.value;
+      let password = $user_password.value;
+
+      if (password.length < 4)
+        return alert("비밀번호는 최소 4글자가 되어야 합니다.");
+
+      const obj = {
+        name,
+        text,
+        password,
+        date: date.toLocaleString("ko-kr"),
+      };
+      const objString = JSON.stringify(obj);
+      localStorage.setItem(`${name}`, objString);
+      getReviews();
+      initInput();
+    } else if (method === "edit") {
+      // 수정일 때
+      const data = JSON.parse(localStorage.getItem(selectId));
+      if (data.password === $user_password.value) {
+        let text = $write_input.value;
+        data.text = text;
+        localStorage.setItem(selectId, JSON.stringify(data));
+        alert("수정이 완료되었습니다.");
+
+        // 수정 후 등록 창으로 변환
+        method = "submit";
+        document.querySelector(".write_button").textContent = "등록";
+        initInput();
+        getReviews();
+      } else {
+        return alert("비밀번호가 일치하지 않습니다.");
+      }
+    }
+  });
+
   // 데이터 가져오기
   function getReviews() {
-    const reviews = [];
+    let reviews = [];
     for (let i = 0; i < window.localStorage.length; i++) {
       // key 찾기
       const key = window.localStorage.key(i);
@@ -31,7 +74,13 @@ window.onload = function () {
       reviews.push(value);
     }
 
+    // 데이터 정렬 및 렌더링
     $item_wrapper.innerHTML = reviews
+      .sort((a, b) => {
+        if (a.date < b.date) return 1;
+        else if (a.date === b.date) return 0;
+        else if (a.date > b.date) return -1;
+      })
       .map((review) => {
         return `<li class="review_card">
                     <div class="review_content">${review.text}</div>
@@ -83,55 +132,13 @@ window.onload = function () {
         const data = JSON.parse(localStorage.getItem(selectId));
         $write_input.value = data.text;
         $user_name.value = data.name;
+        $user_name.setAttribute("readonly", true);
         document.querySelector(".write_button").textContent = "수정";
         $write_input.focus();
         window.scrollTo(0, 0); // 스크롤이 아래 있을 때 수정창으로 바로 이동
       });
     });
   }
-
-  // 댓글 입력창 기능 구현
-  $_content.addEventListener("submit", (event) => {
-    event.preventDefault();
-    if (method === "submit") {
-      // 등록일 때
-      const date = new Date();
-      let text = $write_input.value;
-      let name = $user_name.value;
-      let password = $user_password.value;
-
-      if (password.length < 4)
-        return alert("비밀번호는 최소 4글자가 되어야 합니다.");
-
-      const obj = {
-        name,
-        text,
-        password,
-        date: date.toLocaleString("ko-kr"),
-      };
-      const objString = JSON.stringify(obj);
-      localStorage.setItem(`${name}`, objString);
-      getReviews();
-      initInput();
-    } else if (method === "edit") {
-      // 수정일 때
-      const data = JSON.parse(localStorage.getItem(selectId));
-      if (data.password === $user_password.value) {
-        let text = $write_input.value;
-        data.text = text;
-        localStorage.setItem(selectId, JSON.stringify(data));
-        alert("수정이 완료되었습니다.");
-
-        // 수정 후 등록 창으로 변환
-        method = "submit";
-        document.querySelector(".write_button").textContent = "등록";
-        initInput();
-        getReviews();
-      } else {
-        return alert("비밀번호가 일치하지 않습니다.");
-      }
-    }
-  });
 
   getReviews();
 };
